171d4af70fe505371754d182d1e5661c6625c46d
Thomy <thomyfarhan@gmail.com>
2019-11-30 23:44:13 +0700

    (Model Crystal) - Update Crystal Data

--- app/src/main/java/com/aesthomic/gemdigger/CrystalData.kt
+++ app/src/main/java/com/aesthomic/gemdigger/CrystalData.kt
@@ -14,25 +14,25 @@ object CrystalData {
             R.drawable.crystal_lime, 5, 0
         ),
         Crystal(
-            R.drawable.crystal_red, 35, 17
+            R.drawable.crystal_red, 35, 80
         ),
         Crystal(
-            R.drawable.crystal_purple, 60, 43
+            R.drawable.crystal_purple, 60, 650
         ),
         Crystal(
-            R.drawable.crystal_green, 85, 101
+            R.drawable.crystal_green, 85, 2560
         ),
         Crystal(
-            R.drawable.crystal_blue, 155, 204
+            R.drawable.crystal_blue, 155, 9870
         ),
         Crystal(
-            R.drawable.crystal_orange, 275, 501
+            R.drawable.crystal_orange, 275, 18760
         ),
         Crystal(
-            R.drawable.crystal_aqua, 420, 1007
+            R.drawable.crystal_aqua, 420, 49990
         ),
         Crystal(
-            R.drawable.crystal_fuchsia, 880, 2525
+            R.drawable.crystal_fuchsia, 880, 149990
         )
     )
 }
