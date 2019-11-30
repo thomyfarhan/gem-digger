5737ba5b221108825afffe682b073b703baeeb02
Thomy <thomyfarhan@gmail.com>
2019-11-30 19:02:37 +0700

    (Activity Main) Create condition for enabling transparent Status Bar

--- app/src/main/java/com/aesthomic/gemdigger/MainActivity.kt
+++ app/src/main/java/com/aesthomic/gemdigger/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.aesthomic.gemdigger
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.databinding.DataBindingUtil
 import com.aesthomic.gemdigger.databinding.ActivityMainBinding
 
@@ -12,5 +14,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Check SDK Version for enabling transparent Status Bar
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        }
     }
 }
